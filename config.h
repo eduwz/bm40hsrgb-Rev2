3d5f9a1a0e7b08b0789c25db65dde6633c72a545
eduwz <eduwz-2d@yahoo.com.br>
2023-10-20 16:42:12 -0300

    Update config.h
    vial support

--- config.h
+++ config.h
@@ -7,7 +7,11 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 /* QK_GESC side effects*/
 #define GRAVE_ESC_CTRL_OVERRIDE
-// disabling some RGB Effects to reduce firmware size.
+/* vial support*/
+#define VIAL_KEYBOARD_UID {0x9C, 0x26, 0xB1, 0xAF, 0x7B, 0x91, 0xAC, 0x62}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+/* disabling some RGB Effects to reduce firmware size */
 #undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
 #undef ENABLE_RGB_MATRIX_CYCLE_ALL
