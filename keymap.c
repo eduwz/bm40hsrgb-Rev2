6b8fcdd0275b345be83425d4b5c971c87a53d0d4
eduwz <eduwz-2d@yahoo.com.br>
2023-12-06 23:35:01 -0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -117,22 +117,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      | 
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   [  |   ]  |   (  |   )  |   ~  |   `  |   -  |   +  |   =  |   |  |      |
+ * |      |   [  |   ]  |   (  |   )  |   ~  |   '  |   -  |   +  |   =  |   |  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |   {  |   }  |      |   \  |   _  |   <  |   >  |      |      |
+ * |      |      |      |   {  |   }  |   ^  |   \  |   _  |   <  |   >  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |Adjust|             | ---- |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
 */
 [_RAISE] = LAYOUT_ortho_4x12_1x2uC(
-    KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-    KC_TRNS, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TILD, KC_GRV, KC_MINS, KC_PLUS, KC_EQL,  KC_PIPE, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_BSLS, KC_UNDS, KC_LT,   KC_GT,   KC_TRNS, KC_TRNS,
+    KC_GRV,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+    KC_TRNS, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TILD, KC_QUOT, KC_MINS, KC_PLUS, KC_EQL,  KC_PIPE, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_CIRC, KC_BSLS, KC_UNDS, KC_LT,   KC_GT,   KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, ADJUST,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Functions & Numbers
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * |  TAB |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -142,27 +142,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
 */
 [_FUNMB] = LAYOUT_ortho_4x12_1x2uC(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
     KC_TRNS, KC_F11,  KC_F12,  KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SOUND,   KC_MPRV, KC_MNXT, KC_MPLY
 ),
 /* Game
  * ,-----------------------------------------------------------------------------------.
- * |      |   1  |   Q  |   W  |   E  |   R  |      |      |      |      |   P  |      |
+ * |      |   Q  |   W  |   E  |   R  |   T  |      |      |      |      |   P  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   2  |   A  |   S  |   D  |   F  |   G  |      |      |      |      |      |
+ * | FuNmB|   A  |   S  |   D  |   F  |   G  |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   3  | Shft |   Z  |   X  |   C  |   V  |   B  |   M  |      |      |      |      |
+ * |      |   Z  |   X  |   C  |   V  |   B  |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   4  | Ctrl |   5  |      |      |             |      |      |      |      |      |
+ * |      |   4  |   3  |   2  |   1  |             |      |      |      |      | GAME |
  * `-----------------------------------------------------------------------------------'
  */
 [_GAME] = LAYOUT_ortho_4x12_1x2uC(
-    KC_TRNS, KC_1,    KC_Q, KC_W,    KC_E,    KC_R,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P,    KC_TRNS,
-    KC_TRNS, KC_2,    KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_3,    KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B,    KC_M,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_4,    KC_LCTL, KC_5, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P,    KC_TRNS,
+    FUNMB,   KC_A, KC_S, KC_D, KC_F, KC_G,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_4, KC_3, KC_2, KC_1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, GAME
 ),
 /* Numpad
  * ,-----------------------------------------------------------------------------------.
